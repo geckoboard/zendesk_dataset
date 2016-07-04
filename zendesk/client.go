80040c84431a30004943b35058c383dfd9d9753d
Jon Normington <jnormington@users.noreply.github.com>
2016-07-04 22:40:27 +0000

    Query search api endpoint with support for pagination

--- zendesk/client.go
+++ zendesk/client.go
@@ -1,9 +1,12 @@
 package zendesk
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 
 	"github.com/jnormington/geckoboard_zendesk/conf"
 )
@@ -14,11 +17,23 @@ type Client struct {
 }
 
 var domainURL = "https://%s.zendesk.com/api/v2"
+var client = &http.Client{Timeout: time.Second * 10}
 
 const searchPath = "/search.json"
 
 func (c *Client) buildRequest(method, path, queryParams string) (*http.Request, error) {
-	uri := fmt.Sprintf(domainURL, c.Auth.Subdomain) + path
+	var uri string
+	domain := fmt.Sprintf(domainURL, c.Auth.Subdomain)
+
+	if strings.Contains(path, domain) {
+		uri = path
+		path = ""
+	} else if strings.Contains(queryParams, domain) {
+		uri = queryParams
+		queryParams = ""
+	} else {
+		uri = domain + path
+	}
 
 	if queryParams != "" {
 		qp := "?query="
@@ -39,4 +54,51 @@ func (c *Client) buildRequest(method, path, queryParams string) (*http.Request, 
 	}
 
 	return req, nil
+}
+
+func (c *Client) SearchTickets(queryParams string) (*TicketPayload, error) {
+	res := TicketPayload{}
+	var tp []Ticket
+
+	err := c.searchTickets(queryParams, &tp)
+	if err != nil {
+		return nil, err
+	}
+
+	res.Tickets = tp
+	res.Count = len(tp)
+
+	return &res, nil
+}
+
+func (c *Client) searchTickets(queryParam string, t *[]Ticket) error {
+	req, err := c.buildRequest("GET", searchPath, queryParam)
+
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var tp TicketPayload
+	err = json.NewDecoder(resp.Body).Decode(&tp)
+	if err != nil {
+		return err
+	}
+
+	for _, tck := range tp.Tickets {
+		*t = append(*t, tck)
+	}
+
+	if c.PaginateResults && tp.NextPage != "" {
+		err = c.searchTickets(tp.NextPage, t)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
