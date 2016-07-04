f79cae18549de59bdb57344b234734c5855aea65
Jon Normington <jnormington@users.noreply.github.com>
2016-07-04 00:53:02 +0000

    Build query for search filter

--- models/search_filter.go
+++ models/search_filter.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"sort"
 	"strings"
+	"time"
 )
 
 const (
@@ -18,7 +20,7 @@ var validOperators = [5]string{">", ":", "<", ">=", "<="}
 
 type SearchFilter struct {
 	Type      string
-	DateRange []DateFilter
+	DateRange DateFilters
 	Value     map[string]string
 	Values    map[string][]string
 }
@@ -93,4 +95,81 @@ func keyHasOperator(key string) bool {
 	}
 
 	return false
+}
+
+// BuildQuery builds a valid zendesk search api call
+func (sf *SearchFilter) BuildQuery(t *time.Time) string {
+	var bf bytes.Buffer
+
+	if sf.Type == "" {
+		sf.defaultType()
+	}
+
+	bf.WriteString(fmt.Sprintf("type:%s ", sf.Type))
+	if len(sf.DateRange) != 0 {
+		bf.WriteString(sf.DateRange.BuildQuery(t))
+		bf.WriteString(" ")
+	}
+
+	sf.buildValue(&bf)
+	sf.buildValues(&bf)
+
+	return strings.TrimRight(bf.String(), " ")
+}
+
+func (sf *SearchFilter) buildValue(bf *bytes.Buffer) {
+	// Maps are randomized but we need them ordered at least for the tests
+	keys := []string{}
+	for k := range sf.Value {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+	for _, k := range keys {
+		bf.WriteString(k)
+		val := sf.Value[k]
+		if strings.Contains(val, " ") {
+			bf.WriteString(`"`)
+			bf.WriteString(val)
+			bf.WriteString(`"`)
+		} else {
+			bf.WriteString(val)
+		}
+		bf.WriteString(" ")
+	}
+
+}
+
+func (sf *SearchFilter) buildValues(bf *bytes.Buffer) {
+	keys := []string{}
+	idx := 0
+
+	for k := range sf.Values {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		idx++
+		vals := sf.Values[k]
+
+		for i, y := range vals {
+			bf.WriteString(k)
+			if strings.Contains(y, " ") {
+				bf.WriteString(`"`)
+				bf.WriteString(y)
+				bf.WriteString(`"`)
+			} else {
+				bf.WriteString(y)
+			}
+			if i != len(vals)-1 {
+				bf.WriteString(" ")
+			}
+		}
+
+		if idx != len(sf.Values) {
+			bf.WriteString(" ")
+		}
+	}
 }
--- models/search_filter_test.go
+++ models/search_filter_test.go
@@ -160,3 +160,151 @@ func TestUpdatesTypeToTicket(t *testing.T) {
 	}
 
 }
+
+type BuildQueryTC struct {
+	SF     SearchFilter
+	Output string
+}
+
+func TestSearchFilterBuildQuery(t *testing.T) {
+	testCases := []BuildQueryTC{
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{},
+			},
+			Output: "type:ticket",
+		},
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{},
+				Values: map[string][]string{
+					"groups:": []string{
+						"first_line",
+						"second_line",
+					},
+				},
+			},
+			Output: "type:ticket groups:first_line groups:second_line",
+		},
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{
+					{
+						Unit: month,
+						Past: 2,
+					},
+					{
+						Attribute: "updated",
+						Custom:    "<2017-01-01",
+					},
+				},
+			},
+			Output: "type:ticket created>2016-04-01 updated<2017-01-01",
+		},
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{
+					{
+						Unit: month,
+						Past: 2,
+					},
+					{
+						Attribute: "updated",
+						Custom:    "<2017-01-01",
+					},
+				},
+				Value: map[string]string{
+					"tags:": "beta",
+				},
+			},
+			Output: "type:ticket created>2016-04-01 updated<2017-01-01 tags:beta",
+		},
+		{
+			SF: SearchFilter{
+				Type: "user",
+				DateRange: DateFilters{
+					{
+						Unit: month,
+						Past: 2,
+					},
+					{
+						Attribute: "updated",
+						Custom:    "<2017-01-01",
+					},
+				},
+				Value: map[string]string{
+					"tags:":   "beta not",
+					"status>": "pending",
+				},
+			},
+			Output: `type:user created>2016-04-01 updated<2017-01-01 status>pending tags:"beta not"`,
+		},
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{
+					{
+						Unit: month,
+						Past: 1,
+					},
+				},
+				Values: map[string][]string{
+					"tags:": []string{
+						"expired",
+						"freetrial",
+						"test_user",
+					},
+				},
+			},
+			Output: "type:ticket created>2016-05-01 tags:expired tags:freetrial tags:test_user",
+		},
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{
+					{
+						Attribute: "updated",
+						Custom:    "<2017-01-01",
+					},
+				},
+				Values: map[string][]string{
+					"tags:": []string{
+						"expired",
+					},
+					"status:": []string{
+						"pending",
+						"solved",
+					},
+				},
+			},
+			Output: "type:ticket updated<2017-01-01 status:pending status:solved tags:expired",
+		},
+		{
+			SF: SearchFilter{
+				DateRange: DateFilters{
+					{
+						Attribute: "updated",
+						Custom:    "<2017-01-01",
+					},
+				},
+				Value: map[string]string{
+					"tags:":   "beta urgent",
+					"groups>": "firstline",
+				},
+				Values: map[string][]string{
+					"tags:": []string{
+						"pending solved",
+						"planone",
+					},
+				},
+			},
+			Output: `type:ticket updated<2017-01-01 groups>firstline tags:"beta urgent" tags:"pending solved" tags:planone`,
+		},
+	}
+
+	for _, tc := range testCases {
+		output := tc.SF.BuildQuery(&staticTime)
+
+		if output != tc.Output {
+			t.Errorf("Expected output %s but got %s", tc.Output, output)
+		}
+	}
+}
