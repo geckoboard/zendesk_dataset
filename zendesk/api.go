a31445240c8a6c1f310ff6eed7eedcf6c45f1622
Jon Normington <jnormington@users.noreply.github.com>
2016-07-13 22:10:21 +0000

    Add client support for getting ticket metrics... Using the current search filter ability via the search api. Then utilise the zendesk side loading feature on the tickets endpoint

--- zendesk/api.go
+++ zendesk/api.go
@@ -2,6 +2,18 @@ package zendesk
 
 import "time"
 
+// MetricSet describes a subset of metrics under a ticket.
+type MetricSet struct {
+	ReplyTime          SubTimeMetric `json:"reply_time_in_minutes"`
+	FullResolutionTime SubTimeMetric `json:"full_resolution_time_in_minutes"`
+}
+
+// SubTimeMetric describe metrics with business and calendar values.
+type SubTimeMetric struct {
+	Business int `json:"business"`
+	Calendar int `json:"calendar"`
+}
+
 // TicketPayload the payload returned for search api for type:ticket.
 type TicketPayload struct {
 	Tickets  []Ticket `json:"results"`
@@ -13,5 +25,13 @@ type TicketPayload struct {
 type Ticket struct {
 	ID        int       `json:"id"`
 	Tags      []string  `json:"tags"`
+	Metrics   MetricSet `json:"metric_set"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+// TicketMetrics is the tickets/show_many.json schema.
+type TicketMetrics struct {
+	Tickets  []Ticket `json:tickets`
+	Count    int      `json:"count"`
+	NextPage string   `json:"next_page"`
 }
